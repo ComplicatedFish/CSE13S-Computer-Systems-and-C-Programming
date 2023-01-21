47dcd1960cc9be4491d0ca5840554140175ba190
anepal <anepal@ucsc.edu>
2023-01-21 01:21:38 -0800

    second plot complete

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -18,14 +18,37 @@ gnuplot <<END
 	set size square
 	set tics .2
 	set xlabel "x"
-	set ylabel "Errors"
+	set ylabel "y"
 	set zeroaxis
 	plot "lessthan.dat" with points pt 7 ps .25 lc rgb "blue" title "", \
 	"greaterthan.dat" with points pt 7 ps .25 lc rgb "red" title "", \
 	sqrt(1-(x**2)) with lines lc rgb "black" title ""
 
 END
+for i in {2..6}
+do
+	./monte_carlo -r $i -n 4096 > output.dat
+	awk {' print $2'} < output.dat > temp.dat
+	tail -n +2 temp.dat > estimation.dat 
+	awk '{ print $1 - 3.14159 ;}' estimation.dat > error$i.dat
+done
 
-./monte_carlo -r 2 > output.dat
-awk {' print $2'} < output.dat > temp.dat
-tail -n +2 temp.dat > pi estimation.dat 
+gnuplot <<END
+	set terminal pdf
+	set output "error.pdf"
+	set title "Monte Carlo Error Estimation"
+	set xlabel "x"
+	set ylabel "Errors"
+	set yrange [-1:1]
+	set logscale x 4
+	set ytic .5
+	set zeroaxis
+	plot "error2.dat" with lines title "", \
+	"error3.dat" with lines title "", \
+	"error4.dat" with lines title "", \
+	"error5.dat" with lines title "", \
+	"error6.dat" with lines title ""
+
+
+END
+
