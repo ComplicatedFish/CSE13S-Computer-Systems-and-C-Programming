ead77efd0b49329b655f017874e20a67037dc1ee
anepal <anepal@ucsc.edu>
2023-02-10 22:28:20 -0800

    Universe function but HUGE memory leaks

--- asgn4/life.c
+++ asgn4/life.c
@@ -6,14 +6,18 @@
 #include <stdbool.h>
 #include <limits.h>
 #include <string.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+#define DELAY 50000
 
 #define OPTIONS "tsn:i:o:"
 
 int main(int argc, char **argv){
     bool t = false; //determines toroidality
-    bool s = false; //determines ncurses display
+    bool s = true; //determines ncurses display
     uint32_t n_generations = 100;
-    uint32_t h = false; //help message eligibility
+    //uint32_t h = false; //help message eligibility
     char *input = (char *)malloc(PATH_MAX); //PATH_MAX from limits.h stores maximum path length
     char *output = (char *)malloc(PATH_MAX);
     int opt = 0;
@@ -23,7 +27,7 @@ int main(int argc, char **argv){
                 t = true;
                 break;
             case 's':
-                s = true;
+                s = false; //silences the ncurses display
                 break;
             case 'n':
                 n_generations = strtoul(optarg, NULL, 10);
@@ -35,7 +39,7 @@ int main(int argc, char **argv){
                 output = strcpy(output, optarg);
                 break;
             default:
-                h = 1;
+                //h = true;
                 break;
 
         }
@@ -46,8 +50,61 @@ int main(int argc, char **argv){
 
     FILE *fp;
     fp = fopen(input, "r");
-    fcanf(fp, "%" SCNd32 "%" SCNd32 "", &rows, &cols);
-    Universe *u = uv_create(rows, cols, false);
+    fscanf(fp, "%" SCNd32 "%" SCNd32 "", &rows, &cols);
+    Universe *A = uv_create(rows, cols, t);
+    Universe *B = uv_create(rows, cols, t);
+
+    uv_populate(A, fp);
+    fclose(fp);
+
+    initscr();
+    curs_set(FALSE);
+    for (uint32_t n = 0; n < n_generations; n++){
+        if(s){
+            clear();
+            for (uint32_t r = 0; r < uv_rows(A); r++){
+                for (uint32_t c = 0; c < uv_cols(A); c++){
+                    if (uv_get_cell(A, r, c)){
+                        mvprintw(r, c, "o");
+                    } else {
+                        mvprintw(r, c, " ");
+                    }
+                }
+                mvprintw(r, uv_cols(A), "\n"); //newline occurs after all columns printed
+            }
+            refresh();
+            usleep(DELAY);
+        }
+        for (uint32_t r = 0; r < uv_rows(A); r++){
+            for (uint32_t c = 0; c < uv_cols(A); c++){
+                if (uv_get_cell(A, r, c)){
+                    if (uv_census(A, r, c) == 2 || uv_census(A, r, c) == 3){
+                        uv_live_cell(B, r, c);
+                    } else {
+                        uv_dead_cell(B, r, c);
+                    }
+                } else {
+                    if (uv_census(A, r, c) == 3){
+                        uv_live_cell(B, r, c);
+                    }
+                }
+            }
+
+        }
+        Universe *temp = A;
+        A = B;
+        B = temp;
+    }
+    endwin();
+
+    FILE *out;
+    out = fopen(output, "w");
+    uv_print(A, out);
+    fclose(out);
+
+
+
+/*
     printf("%"PRIu32" rows, %"PRIu32" cols\n", uv_rows(u), uv_cols(u));
 
     uv_live_cell(u, 4, 4);
@@ -75,9 +132,25 @@ int main(int argc, char **argv){
     printf("toroid: %d\n", t);
     printf("n : %u\n", n_generations);
     printf("ncurses: %d\n", s);
+*/
 
-
-    uv_delete(u);
+    uv_delete(A);
+    uv_delete(B);
+    free(output);
+    free(input);
     return 0;
 }
+/*
+int main ( void ) {
+    initscr () ;            // Initialize the screen .
+    curs_set ( FALSE ) ;    // Hide the cursor .
+    for ( int col = 0; col < 40; col += 1) {
+        clear () ;          // Clear the window .
+        mvprintw (ROW , col , "o") ; // Displays "o".
+        refresh () ;        // Refresh the window .
+        usleep ( DELAY ) ;  // Sleep for 50000 microseconds .
+    }
+    endwin () ;             // Close the screen .
+    return 0;
+}*/
 
