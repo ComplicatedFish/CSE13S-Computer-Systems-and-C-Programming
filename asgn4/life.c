9f8466aa351f929b8cad1ea50ffa170c7bf9536d
anepal <anepal@ucsc.edu>
2023-02-12 20:21:31 -0800

    clang format all c files

--- asgn4/life.c
+++ asgn4/life.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "universe.h"
 #include "inttypes.h"
-#include <stdbool.h>
+#include "universe.h"
+
 #include <limits.h>
+#include <ncurses.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <ncurses.h>
 
 #define DELAY 50000
 
@@ -18,7 +18,7 @@
 //program
 void print_help(void);
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     bool t = false; //determines toroidality
     bool s = true; //determines ncurses display
     uint32_t n_generations = 100;
@@ -27,33 +27,20 @@ int main(int argc, char **argv){
     FILE *output = stdout;
     int opt = 0;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
-        switch(opt) {
-            case 't':
-                t = true;
-                break;
-            case 's':
-                s = false; //silences the ncurses display
-                break;
-            case 'n':
-                n_generations = strtoul(optarg, NULL, 10);
-                break;
-            case 'i':
-                input = fopen(optarg, "r");
-                break;
-            case 'o':
-                output = fopen(optarg, "w");
-                break;
-            case 'h':
-                h = true;
-                break;
-            default:
-                h = true;
-                break;
-
+        switch (opt) {
+        case 't': t = true; break;
+        case 's':
+            s = false; //silences the ncurses display
+            break;
+        case 'n': n_generations = strtoul(optarg, NULL, 10); break;
+        case 'i': input = fopen(optarg, "r"); break;
+        case 'o': output = fopen(optarg, "w"); break;
+        case 'h': h = true; break;
+        default: h = true; break;
         }
     }
 
-    if(h){
+    if (h) {
         print_help();
         fclose(input);
         fclose(output);
@@ -70,17 +57,17 @@ int main(int argc, char **argv){
     uv_populate(A, input);
     fclose(input);
 
-    if(s){
+    if (s) {
         initscr();
         curs_set(FALSE);
     }
 
-    for (uint32_t n = 0; n < n_generations; n++){
-        if(s){
+    for (uint32_t n = 0; n < n_generations; n++) {
+        if (s) {
             clear();
-            for (uint32_t r = 0; r < uv_rows(A); r++){
-                for (uint32_t c = 0; c < uv_cols(A); c++){
-                    if (uv_get_cell(A, r, c)){
+            for (uint32_t r = 0; r < uv_rows(A); r++) {
+                for (uint32_t c = 0; c < uv_cols(A); c++) {
+                    if (uv_get_cell(A, r, c)) {
                         mvprintw(r, c, "o");
                     } else {
                         mvprintw(r, c, " ");
@@ -91,30 +78,29 @@ int main(int argc, char **argv){
             refresh();
             usleep(DELAY);
         }
-        for (uint32_t r = 0; r < uv_rows(A); r++){
-            for (uint32_t c = 0; c < uv_cols(A); c++){
-                if (uv_get_cell(A, r, c)){
-                    if (uv_census(A, r, c) == 2 || uv_census(A, r, c) == 3){
+        for (uint32_t r = 0; r < uv_rows(A); r++) {
+            for (uint32_t c = 0; c < uv_cols(A); c++) {
+                if (uv_get_cell(A, r, c)) {
+                    if (uv_census(A, r, c) == 2 || uv_census(A, r, c) == 3) {
                         uv_live_cell(B, r, c);
                     } else {
                         uv_dead_cell(B, r, c);
                     }
                 } else {
-                    if (uv_census(A, r, c) == 3){
+                    if (uv_census(A, r, c) == 3) {
                         uv_live_cell(B, r, c);
                     } else {
                         uv_dead_cell(B, r, c);
                     }
                 }
             }
-
         }
         Universe *temp;
         temp = A;
         A = B;
         B = temp;
     }
-    if(s){
+    if (s) {
         endwin();
     }
 
@@ -128,18 +114,18 @@ int main(int argc, char **argv){
 }
 
 void print_help(void) {
-    fprintf(stderr, "SYNOPSIS\n"
-                    "   An implementation of John Horton Conway's Game of life.\n"
-                    "\n"
-                    "USAGE\n"
-                    "   ./life [-i:o:n:st]\n"
-                    "\n"
-                    "OPTIONS\n"
-                    "   -t              Specifies a toroidal universe.\n"
-                    "   -s              Silences the ncurses animation and window.\n"
-                    "   -n generations  Specifies the number of generations to undergo in game of life.\n"
-                    "   -i input        Specifies the input file to read from.\n"
-                    "   -o output       Specifies the out file to write to.\n");
+    fprintf(stderr,
+        "SYNOPSIS\n"
+        "   An implementation of John Horton Conway's Game of life.\n"
+        "\n"
+        "USAGE\n"
+        "   ./life [-i:o:n:st]\n"
+        "\n"
+        "OPTIONS\n"
+        "   -t              Specifies a toroidal universe.\n"
+        "   -s              Silences the ncurses animation and window.\n"
+        "   -n generations  Specifies the number of generations to undergo in game of life.\n"
+        "   -i input        Specifies the input file to read from.\n"
+        "   -o output       Specifies the out file to write to.\n");
     return;
 }
-
--- asgn4/universe.c
+++ asgn4/universe.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include "universe.h"
+
 #include "inttypes.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 struct Universe {
     uint32_t rows;
@@ -19,12 +21,12 @@ struct Universe {
 //the struct to the array that has been initialized. Returns pointer
 //to Universe struct
 
-Universe *uv_create(uint32_t rows, uint32_t cols, bool toroidal){
-    Universe *uv = (Universe *)calloc(4, sizeof(Universe));
+Universe *uv_create(uint32_t rows, uint32_t cols, bool toroidal) {
+    Universe *uv = (Universe *) calloc(4, sizeof(Universe));
     uv->rows = rows;
     uv->cols = cols;
     bool **grid = (bool **) calloc(rows, sizeof(bool *));
-    for (uint32_t i = 0; i < rows; i++){
+    for (uint32_t i = 0; i < rows; i++) {
         grid[i] = (bool *) calloc(cols, sizeof(bool));
     }
     uv->toroidal = toroidal;
@@ -37,8 +39,8 @@ Universe *uv_create(uint32_t rows, uint32_t cols, bool toroidal){
 //memory within (since matrices are arrays of pointers). Then free()'s
 //the grid memory. Also frees the struct memory.
 
-void uv_delete(Universe *u){
-    for(uint32_t i = 0; i < u->rows; i++){
+void uv_delete(Universe *u) {
+    for (uint32_t i = 0; i < u->rows; i++) {
         free(u->grid[i]);
         u->grid[i] = NULL;
     }
@@ -50,38 +52,38 @@ void uv_delete(Universe *u){
 
 //simple function, returns rows cotained within
 //Universe u
-uint32_t uv_rows(Universe *u){
+uint32_t uv_rows(Universe *u) {
     return u->rows;
 }
 
 //returns columns contained within Universe u
-uint32_t uv_cols(Universe *u){
+uint32_t uv_cols(Universe *u) {
     return u->cols;
 }
 
 //sets the cell in the u->grid at row r and column c
 //to true (to emulate a live cell in the game of life).
 //returns
-void uv_live_cell(Universe *u, uint32_t r, uint32_t c){
+void uv_live_cell(Universe *u, uint32_t r, uint32_t c) {
     //if (r < 0 && c < 0) {return;}
-    if (r < u->rows && c < u->cols){
+    if (r < u->rows && c < u->cols) {
         u->grid[r][c] = true;
     }
 }
 
 //ooposite of uv_live_cell(), sets the cell at row r and
 //column c to false (deal cell)
-void uv_dead_cell(Universe *u, uint32_t r, uint32_t c){
+void uv_dead_cell(Universe *u, uint32_t r, uint32_t c) {
     //if (r < 0 && c < 0) {return;}
-    if (r < u->rows && c < u->cols){
+    if (r < u->rows && c < u->cols) {
         u->grid[r][c] = false;
     }
 }
 
 //uses same conditions as uv_live_cell() to get the value
 //of the cell. If out of bounds, returns false.
-bool uv_get_cell(Universe *u, uint32_t r, uint32_t c){
-    if (/*r >= 0 && c >= 0 && */r < u->rows && c < u->cols){
+bool uv_get_cell(Universe *u, uint32_t r, uint32_t c) {
+    if (/*r >= 0 && c >= 0 && */ r < u->rows && c < u->cols) {
         return u->grid[r][c];
     } else {
         return false;
@@ -93,11 +95,11 @@ bool uv_get_cell(Universe *u, uint32_t r, uint32_t c){
 //loop checks if uv_live_cell() worked. Function as a boundary checker,
 //since uv_live_cell() will only fail to make a cell if the cell index
 //is of bounds
-bool uv_populate(Universe *u, FILE *infile){
+bool uv_populate(Universe *u, FILE *infile) {
     uint32_t x, y;
-    while (fscanf(infile, "%" SCNd32 "%" SCNd32 "", &x, &y) != EOF){
+    while (fscanf(infile, "%" SCNd32 "%" SCNd32 "", &x, &y) != EOF) {
         uv_live_cell(u, x, y);
-        if (uv_get_cell(u, x, y) == false){
+        if (uv_get_cell(u, x, y) == false) {
             return false;
         }
     }
@@ -110,41 +112,45 @@ bool uv_populate(Universe *u, FILE *infile){
 //the universe "wraps around" on itself. To check indexes, I used
 //2 for loops that loop over the 8 block area around the cell to be
 //censused.
-uint32_t uv_census(Universe *u, uint32_t r, uint32_t c){
+uint32_t uv_census(Universe *u, uint32_t r, uint32_t c) {
     uint32_t num_neighbors = 0;
     int32_t row_index;
     int32_t col_index;
-    if (u->toroidal){
-        for (int32_t i = -1; i <= 1; i++){
-            for (int32_t j = -1; j <= 1; j++){
-                if (j == 0 && i == 0){continue;} //cell being censused doesn't count
+    if (u->toroidal) {
+        for (int32_t i = -1; i <= 1; i++) {
+            for (int32_t j = -1; j <= 1; j++) {
+                if (j == 0 && i == 0) {
+                    continue;
+                } //cell being censused doesn't count
 
                 //since C can't do arithmetic modulo (where -1 mod 5 = 4) we have to account
                 //for cases where we have to census cells in row 0 or column 0. This means
                 //we have to subtract 1 from the num_rows and num_cols to obtain the index
                 //at u->rows-1 that is required to be checked when toroidality is enabled
-                if (r == 0 && i == -1){
-                    row_index = (int32_t)u->rows - 1;
+                if (r == 0 && i == -1) {
+                    row_index = (int32_t) u->rows - 1;
                 } else {
-                    row_index = ((int32_t)r+i)%((int32_t)u->rows);
+                    row_index = ((int32_t) r + i) % ((int32_t) u->rows);
                 }
 
-                if (c == 0 && j == -1){
-                    col_index = (int32_t)u->cols - 1;
+                if (c == 0 && j == -1) {
+                    col_index = (int32_t) u->cols - 1;
                 } else {
-                    col_index = ((int32_t)c+j)%((int32_t)u->cols);
+                    col_index = ((int32_t) c + j) % ((int32_t) u->cols);
                 }
 
-                if (uv_get_cell(u, row_index, col_index)){
+                if (uv_get_cell(u, row_index, col_index)) {
                     num_neighbors++;
                 }
             }
         }
     } else { //normal case
-        for (int i = -1; i <= 1; i++){
-            for (int j = -1; j <= 1; j++){
-                if (j == 0 && i == 0){continue;} //cell being censused doesn't count
-                if (uv_get_cell(u, ((int32_t)r+i), ((int32_t)c+j))){
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (j == 0 && i == 0) {
+                    continue;
+                } //cell being censused doesn't count
+                if (uv_get_cell(u, ((int32_t) r + i), ((int32_t) c + j))) {
                     num_neighbors++;
                 }
             }
@@ -154,10 +160,10 @@ uint32_t uv_census(Universe *u, uint32_t r, uint32_t c){
 }
 
 //prints the grid within universe u cell by cell using fputc
-void uv_print(Universe *u, FILE *outfile){
-    for (uint32_t r = 0; r < u->rows; r++){
-        for (uint32_t c = 0; c < u->cols; c++){
-            if(uv_get_cell(u, r, c)){
+void uv_print(Universe *u, FILE *outfile) {
+    for (uint32_t r = 0; r < u->rows; r++) {
+        for (uint32_t c = 0; c < u->cols; c++) {
+            if (uv_get_cell(u, r, c)) {
                 fputc(111, outfile); //111 is ASCII for lowercase o
             } else {
                 fputc(46, outfile); //46 is ASCII for period
@@ -167,4 +173,3 @@ void uv_print(Universe *u, FILE *outfile){
     }
     return;
 }
-
