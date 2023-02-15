48b8f6d496d716a6117eda61f05998e37df8c883
anepal <anepal@ucsc.edu>
2023-02-14 23:56:49 -0800

    initial commit of empty source files, excluding randstate which is finished + Makefile finished

---
+++ asgn5/randstate.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <gmp.h>
+#include <stdlib.h>
+
+gmp_randstate_t state;
+
+void randstate_init(uint64_t seed){
+    gmp_randseed_ui(state, seed);
+    srandom(seed);
+    gmp_randinit_mt(state);
+    return;
+}
+
+void randstate_clear(void){
+    gmp_randclear(state);
+    return;
+}
